835084c474cd81b6ce63b2b9bdbb798f5ecaebd7
Daniel <usechedaniel88@gmail.com>
2023-01-20 15:08:15 +0100

    ingredient ready

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import * as dotenv from 'dotenv';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RecipieModule } from './recipie/recipie.module';
+import { IngredientModule } from './ingredient/ingredient.module';
 dotenv.config()
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGODB_URL),
     RecipieModule,
+    IngredientModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/ingredient/ingredient.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateIngredientDto } from './dto/create-ingredient.dto';
+import { UpdateIngredientDto } from './dto/update-ingredient.dto';
+import { Ingredient } from "./schemas/ingredient.schema";
+
+@Injectable()
+export class IngredientService {
+  
+  constructor( 
+    @InjectModel(Ingredient.name) private readonly ingredientModel: Model<Ingredient>, 
+  ) {} 
+  
+  
+  create(createIngredientDto: CreateIngredientDto) {
+    return 'This action adds a new ingredient';
+  }
+
+  findAll() {
+    return this.ingredientModel.find();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} ingredient`;
+  }
+
+  update(id: number, updateIngredientDto: UpdateIngredientDto) {
+    return `This action updates a #${id} ingredient`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} ingredient`;
+  }
+}
