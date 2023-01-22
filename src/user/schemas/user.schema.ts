a1003989e99be9257b2b5482f2d99fb7d7bcb6ec
Sergi <sergialsina81@gmail.com>
2023-01-22 22:32:09 +0100

    user.schema added

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,1 +1,46 @@
-export class User {}
+import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+    @Prop()
+    fisrt_name: string;
+
+    @Prop()
+    last_name: string;
+
+    @Prop()
+    user_name: string;
+
+    @Prop()
+    email: string;
+
+    @Prop()
+    password: string;
+
+    @Prop()
+    role: string;
+
+    @Prop([Object])
+    favorites: Array<any>;
+
+    @Prop()
+    address: string;
+
+    @Prop([Object])
+    badges: Array<any>;
+
+    @Prop([Object])
+    subscriptions_list: Array<any>;
+
+    @Prop([Object])
+    notifications_list: Array<any>;
+
+    @Prop([Object])
+    groups: Array<any>;
+
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './schemas/user.schema';
 
 @Injectable()
 export class UserService {
+
+  constructor(
+    @InjectModel(User.name) private readonly recipieModel: Model<User>,
+  ) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
