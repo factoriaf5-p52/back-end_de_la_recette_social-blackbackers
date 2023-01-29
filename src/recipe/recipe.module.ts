34f54d0c73cb9055dd18cae6b678b27c83b680b2
Joel <joelv2@gmail.com>
2023-01-29 23:12:54 +0100

    hotfix

--- src/recipe/recipe.module.ts
+++ src/recipe/recipe.module.ts
@@ -3,6 +3,8 @@ import { RecipeService } from './recipe.service';
 import { RecipeController } from './recipe.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RecipeSchema, Recipe } from './schemas/recipe.schema';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
